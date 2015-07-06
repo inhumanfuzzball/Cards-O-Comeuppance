f13a22033b67b3b38a98af3359a9aeafe11414d1
unknown <SansomD@NOT001547.pii.pxl.int>
2015-07-06 16:19:38 +0100

    Initial Code

---
+++ code.js
@@ -0,0 +1,31 @@
+var SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxl425g7nwPAfSsH-Aw27RpwSYcLy5rSCfvt13vrgxhvBP5SOs/exec";
+
+$(document).ready(function() {
+    $.getJSON(SCRIPT_URL+"?callback=?",
+		function (data) { refresh(data); });
+});
+
+function IncrementScore(column){
+	
+	 $.ajax({
+        url: SCRIPT_URL+"?col="+column,
+        type: "POST",
+        crossDomain: true,
+        dataType: "json",
+        success:function(result){
+            refresh(result);
+        },
+        error:function(xhr,status,error){
+            alert(error);
+        }
+    });
+	
+	
+}
+
+function refresh(data){
+	jQuery.each(data, function(index, value) {
+		var control = '#' + this.column;
+		$(control).val(this.value);
+	});
+}
